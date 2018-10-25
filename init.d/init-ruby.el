a3a1852c780dcef61f188ca6503344018745d8ab
Dominic Sisneros <dsisnero@gmail.com>
2018-10-25 07:45:46 -0600

    added yard-mode. Comment out robe mode

--- init.d/init-ruby.el
+++ init.d/init-ruby.el
@@ -38,6 +38,36 @@
 (use-package rspec-mode
   :ensure t)
 
+
+(use-package yard-mode
+  :ensure t
+  :config
+  (progn
+      (defun yard-paragraph-boundarise()
+      (interactive)
+      (setq paragraph-separate "[ \t]*#[ \t]*$")
+      (setq paragraph-start
+      (concatenate
+       'string
+       "^[ \t]*"         ; some whitespace
+       "#[ \t]*"         ; a # character followed by whitespace
+       "\\("
+         "@[[:alpha:]]+" ; a YARD tag
+       "\\|"             ; or
+         "-"             ; a list item
+       "\\)"
+       "\\([ \t]+.*\\)?" ; an optional text
+       "[ \t]*$"         ; some more whitespace
+
+
+       ))
+      (add-hook 'enh-ruby-mode-hook
+                '(lambda ()
+                   (yard-paragraph-boundarise)))
+      (add-hook 'ruby-mode-hook
+                '(lambda ()
+                   (yard-paragraph-boundaries))))))
+
 (use-package smartparens
   :ensure t
   :diminish (smartparens-mode .  "()")
@@ -51,27 +81,28 @@
   (add-hook 'ruby-mode-hook 'rubocop-mode)
   :diminish rubocop-mode)
 
-(use-package robe
-  :ensure t
-  ;;:bind ("C-M-." . robe-jump)
-  :config
-  (add-hook 'ruby-mode-hook
-            '(lambda ()
-               (robe-mode)
-               (rope-ac-setup)))
+;; (use-package robe
+;;   :ensure t
+;;   ;;:bind ("C-M-." . robe-jump)
+;;   :config
+;;   (add-hook 'ruby-mode-hook
+;;             '(lambda ()
+;;                (robe-mode)
+;;                (rope-ac-setup)))
 
-  (setq inf-ruby-default-implementation "pry"
-        inf-ruby-eval-binding "Pry.toplevel_binding")
-  (use-package ac-robe)
-  (use-package inf-ruby :ensure t)
-  (use-package ac-inf-ruby
-    :ensure t
-    :config
-    (eval-after-load 'auto-complete
-      '(add-to-list 'ac-modes 'inf-ruby-mode))
-    (add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
-    )
-  )
+;;   (setq inf-ruby-default-implementation "pry"
+;;         inf-ruby-eval-binding "Pry.toplevel_binding")
+;;   (use-package ac-robe
+;;     :ensure t)
+;;   (use-package inf-ruby :ensure t)
+;;   (use-package ac-inf-ruby
+;;     :ensure t
+;;     :config
+;;     (eval-after-load 'auto-complete
+;;       '(add-to-list 'ac-modes 'inf-ruby-mode))
+;;     (add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
+;;     )
+;;   )
 
 ;; (use-package company
 ;;   :no-require t
