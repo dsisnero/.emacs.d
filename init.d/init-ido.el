527109f58bfaa5d064074065d87e51b3fc17d528
Dominic Sisneros <dsisnero@gmail.com>
2018-10-25 07:30:15 -0600

    don't use ido- use helm instead

--- init.d/init-ido.el
+++ init.d/init-ido.el
@@ -1,26 +1,26 @@
-(require 'req-package)
+;; (require 'req-package)
 
-(req-package ido
-  :ensure t
-  :commands (ido-switch-buffer)
-  :config
-  (ido-mode 1)
-  (ido-everywhere 1))
+;; (req-package ido
+;;   :ensure t
+;;   :commands (ido-switch-buffer)
+;;   :config
+;;   (ido-mode 1)
+;;   (ido-everywhere 1))
 
-(req-package flx
-  :ensure t)
+;; (req-package flx
+;;   :ensure t)
 
-(req-package flx-ido
-  :ensure t
-  :require flx ido
-  :config
-  (flx-ido-mode 1)
-  (setq ido-enable-flex-matching t)
-  (setq ido-use-faces nil))
+;; (req-package flx-ido
+;;   :ensure t
+;;   :require flx ido
+;;   :config
+;;   (flx-ido-mode 1)
+;;   (setq ido-enable-flex-matching t)
+;;   (setq ido-use-faces nil))
 
-(req-package ido-at-point
-  :ensure t
-  :require ido
-  :config (ido-at-point-mode 1))
+;; (req-package ido-at-point
+;;   :ensure t
+;;   :require ido
+;;   :config (ido-at-point-mode 1))
 
-(provide 'init-ido)
+;; (provide 'init-ido)
