4d060e2af4e3770f41941afb8c34e080c38a36ac
Dominic Sisneros <dsisnero@gmail.com>
2018-11-01 18:01:39 -0600

    removed file not needed

--- init.d/init-general.el
+++
@@ -1,6 +0,0 @@
-(use-package flyspell
-  :config
-  (setq ispell-program-name "aspell" ; use aspell instead of ispell
-        ispell-extra-args '("--sug-mode=ultra"))
-  (add-hook 'text-mode-hook #'flyspell-mode)
-  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
